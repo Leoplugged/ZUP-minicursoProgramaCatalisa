7b043a43b88701199de9929fafff560bb24dec41
Leandro Miguel <leandro.miguel@outlook.com>
2022-01-14 19:04:24 -0300

    Consertar script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#name');
+const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
 
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() * 671);
+}
 
 pegarPersonagem = () => {
-    return fetch(`https://rickandmortyapi.com/api/character/30`, {
-        method: 'GET',
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
         headers: {
             Accept: 'application/json',
             "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
-        image.alt = data.name;
+        imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name;
         especie.innerHTML = data.species;
         condicao.innerHTML = data.status;
